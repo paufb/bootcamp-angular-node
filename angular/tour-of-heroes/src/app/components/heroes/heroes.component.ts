72179b48e5611a528952c20fa22d45ad4446df87
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-12 09:51:52 +0200

    tour-of-heroes: Display heroes list

--- angular/tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular/tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,17 +1,20 @@
-import { UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { heroes } from '../../constants/heroes';
 import { Hero } from '../../models/hero.interface';
 
 @Component({
   selector: 'app-heroes',
-  imports: [FormsModule, UpperCasePipe],
+  imports: [FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrl: './heroes.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroesComponent {
-  protected hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  protected HEROES = heroes;
+  protected readonly selectedHero = signal<Hero | null>(null);
+
+  protected onSelectHero(hero: Hero) {
+    this.selectedHero.set(hero);
+  }
 }
