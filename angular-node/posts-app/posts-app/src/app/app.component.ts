7a7131b8dbba0598384490d6b4107dd00f416d24
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-13 11:11:08 +0200

    posts-app: Set app layout

--- angular-node/posts-app/posts-app/src/app/app.component.ts
+++ angular-node/posts-app/posts-app/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule, RouterOutlet, SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'posts-app';
+  protected isSidenavCollapsed = signal(false);
+  protected sidenavWidth = computed(() => this.isSidenavCollapsed() ? '4rem' : '15rem');
 }
