516f5e2c1a4dda8597227f55063faa9bebbb6179
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-14 17:23:46 +0200

    server: Add GET /api/posts

--- angular-node/server/src/app.ts
+++ angular-node/server/src/app.ts
@@ -1,7 +1,26 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { Post } from './models/post.interface';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.get('/api/posts', (req: Request, res: Response) => {
+  const posts: Post[] = Array.from({ length: 5 }).map((_, index) => ({
+    id: index,
+    createdAt: new Date(index),
+    title: `Post title ${index}`,
+    body: `Post body ${index}`
+  }));
+  res.json(posts);
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
---
+++ angular-node/server/src/models/post.interface.ts
@@ -0,0 +1,6 @@
+export interface Post {
+  id: number;
+  createdAt: Date;
+  title: string;
+  body: string;
+}
