30f713acb3242732a03c90ac7b1fbad4c829982b
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-15 09:28:25 +0200

    server: GET /posts - Support 'user' query param.

--- angular-node/server/src/app.ts
+++ angular-node/server/src/app.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from 'express';
 import { Post } from './models/post.interface';
+import { User } from './models/user.interface';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const posts: Post[] = Array.from({ length: 5 }).map((_, index) => ({
+  id: index,
+  createdAt: new Date(index),
+  title: `Post title ${index}`,
+  body: `Post body ${index}`
+}));
+
+const users: User[] = Array.from({ length: 5 }).map((_, index) => ({
+  id: index,
+  profileName: `ProfileName${index}`,
+  userName: `username${index}`
+}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,13 +26,9 @@ app.use((req, res, next) => {
 });
 
 app.get('/api/posts', (req: Request, res: Response) => {
-  const posts: Post[] = Array.from({ length: 5 }).map((_, index) => ({
-    id: index,
-    createdAt: new Date(index),
-    title: `Post title ${index}`,
-    body: `Post body ${index}`
-  }));
-  res.json(posts);
+  let result = posts;
+  if (req.query.user) result = result.map((post, index) => ({ ...post, user: users[index] }));
+  res.json(result);
 });
 
 
