49609db97e23b59460b58a87609981e40ee60f44
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-20 09:32:56 +0200

    posts-app: Provide Angular async animations

--- angular-node/posts-app/src/app/app.config.ts
+++ angular-node/posts-app/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimationsAsync()
   ]
 };
