2c228016f471e9616a83bb731da43e27afd057de
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-06-09 09:39:00 +0200

    posts-app: Add TimeAgoPipe

--- angular-node/posts-app/src/app/shared/components/post-reply/post-reply.component.ts
+++ angular-node/posts-app/src/app/shared/components/post-reply/post-reply.component.ts
@@ -1,13 +1,13 @@
-import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { IPostReply } from '../../interfaces/post-reply.interface';
+import { TimeAgoPipe } from '../../pipes/time-ago.pipe';
 import { ProfilePictureComponent } from '../profile-picture/profile-picture.component';
 
 @Component({
   selector: 'app-post-reply',
-  imports: [DatePipe, MatCard, MatCardContent, ProfilePictureComponent, RouterLink],
+  imports: [MatCard, MatCardContent, ProfilePictureComponent, RouterLink, TimeAgoPipe],
   templateUrl: './post-reply.component.html',
   styleUrl: './post-reply.component.scss'
 })
