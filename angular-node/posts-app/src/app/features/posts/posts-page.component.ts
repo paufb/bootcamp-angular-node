ba88cd3b75ce6886d2919c05e06c212777349bb2
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-06-11 09:29:41 +0200

    posts-app: PostsPageComponent - Do not clear post feed on NavigationEnd

--- angular-node/posts-app/src/app/features/posts/posts-page.component.ts
+++ angular-node/posts-app/src/app/features/posts/posts-page.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
-import { filter } from 'rxjs';
 import { fadeIn, fadeOut, scaleFadeInFromTop } from '../../shared/animations';
 import { PostPromptComponent } from '../../shared/components/post-prompt/post-prompt.component';
 import { IPost } from '../../shared/interfaces/post.interface';
@@ -16,19 +15,12 @@ import { PostFeedService } from '../../shared/services/post-feed.service';
   styleUrl: './posts-page.component.scss',
   animations: [fadeIn, fadeOut, scaleFadeInFromTop]
 })
-export class PostsPageComponent implements OnInit {
+export class PostsPageComponent {
   private readonly postFeedService = inject(PostFeedService);
-  private readonly router = inject(Router);
   protected readonly navLinks = [
     { href: '/posts/all', name: 'All' },
     { href: '/posts/following', name: 'Following' }
   ];
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(_ => this.postFeedService.clearPosts());
-  }
 
   protected onCreatePost(post: IPost) {
     this.postFeedService.prependPost(post);
