bcd984f9954d7e2f032dd79b73a744011e4d5e0d
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-06-13 13:10:49 +0200

    posts-app: Disable Post form submission if invalid

--- angular-node/posts-app/src/app/shared/components/post-prompt/post-prompt.component.ts
+++ angular-node/posts-app/src/app/shared/components/post-prompt/post-prompt.component.ts
@@ -1,20 +1,26 @@
 import { Location } from '@angular/common';
 import { booleanAttribute, ChangeDetectionStrategy, Component, inject, input, output, signal, viewChild } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButton } from '@angular/material/button';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthService } from '../../../core/auth/auth.service';
-import { ICreatePostDTO } from '../../interfaces/create-post-dto.interface';
 import { IPost } from '../../interfaces/post.interface';
 import { PostService } from '../../services/post.service';
 import { ProfilePictureComponent } from '../profile-picture/profile-picture.component';
 
+class NoErrorErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return false;
+  }
+}
+
 @Component({
   selector: 'app-post-prompt',
-  imports: [FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, ProfilePictureComponent, ReactiveFormsModule],
+  imports: [FormsModule, MatButton, MatFormFieldModule, MatInputModule, MatProgressSpinnerModule, ProfilePictureComponent, ReactiveFormsModule],
   templateUrl: './post-prompt.component.html',
   styleUrl: './post-prompt.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -27,16 +33,17 @@ export class PostPromptComponent {
   private readonly location = inject(Location);
   private readonly router = inject(Router);
   private readonly formDirective = viewChild.required(FormGroupDirective);
+  protected readonly noErrorErrorStateMatcher = new NoErrorErrorStateMatcher();
   protected readonly isSubmitting = signal(false);
   protected readonly authenticatedUser = this.authService.authenticatedUser;
   protected readonly formGroup = new FormGroup({
-    body: new FormControl('', Validators.required)
+    body: new FormControl('', { nonNullable: true, validators: [Validators.required] })
   });
 
   protected onSubmit() {
     if (!this.formGroup.valid) return;
     this.isSubmitting.set(true);
-    const dto = this.formGroup.value as ICreatePostDTO;
+    const dto = this.formGroup.getRawValue();
     this.postService.createPost(dto)
       .subscribe({
         next: post => {
