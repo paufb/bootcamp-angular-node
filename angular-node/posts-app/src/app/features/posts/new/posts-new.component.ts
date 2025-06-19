2f3d8ae701eac8231ec325609de2578285497457
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-06-19 10:17:27 +0200

    posts-app: Fix back navigation after successful Post creation

--- angular-node/posts-app/src/app/features/posts/new/posts-new.component.ts
+++ angular-node/posts-app/src/app/features/posts/new/posts-new.component.ts
@@ -1,29 +1,22 @@
 import { Location } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 import { PostPromptComponent } from '../../../shared/components/post-prompt/post-prompt.component';
-import { IPost } from '../../../shared/interfaces/post.interface';
-import { PostService } from '../../../shared/services/post.service';
 
 @Component({
   selector: 'app-posts-new',
-  imports: [FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, PostPromptComponent],
+  imports: [PostPromptComponent],
   templateUrl: './posts-new.component.html',
   styleUrl: './posts-new.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostsNewComponent {
-  private readonly postService = inject(PostService);
   private readonly location = inject(Location);
+  private readonly router = inject(Router);
 
-  protected onCreatePost(newPost: IPost) {
-    this.postService.createPost(newPost)
-      .subscribe({
-        error: error => window.alert(`Could not create post: ${error.message}`),
-        complete: () => this.location.back()
-      });
+  protected onCreatePost() {
+    const { navigationId } = this.location.getState() as any;
+    if (navigationId > 1) return this.location.back();
+    this.router.navigate(['/posts']);
   }
 }
--- angular-node/posts-app/src/app/features/posts/posts-page.component.ts
+++ angular-node/posts-app/src/app/features/posts/posts-page.component.ts
@@ -1,25 +1,23 @@
 import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
-import { fadeIn, fadeOut, scaleFadeInFromTop } from '../../shared/animations';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatFabAnchor } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatTabLink, MatTabNav } from '@angular/material/tabs';
 import { PostPromptComponent } from '../../shared/components/post-prompt/post-prompt.component';
 import { IPost } from '../../shared/interfaces/post.interface';
 import { PostFeedService } from '../../shared/services/post-feed.service';
 
 @Component({
   selector: 'app-posts-page',
-  imports: [MatButtonModule, MatIconModule, MatTabsModule, PostPromptComponent, RouterModule],
+  imports: [MatFabAnchor, MatIcon, MatTabLink, MatTabNav, PostPromptComponent, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './posts-page.component.html',
-  styleUrl: './posts-page.component.scss',
-  animations: [fadeIn, fadeOut, scaleFadeInFromTop]
+  styleUrl: './posts-page.component.scss'
 })
 export class PostsPageComponent {
   private readonly postFeedService = inject(PostFeedService);
   protected readonly navLinks = [
-    { href: '/posts/all', name: 'All' },
-    { href: '/posts/following', name: 'Following' }
+    { href: 'all', name: 'All' },
+    { href: 'following', name: 'Following' }
   ];
 
   protected onCreatePost(post: IPost) {
